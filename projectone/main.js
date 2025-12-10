47480d23240e97a6c7b0828472a4aa4095fe2d82
Chris Gilchrist <chris@quix.ai>
2025-12-10 13:22:51 +0000

    Tesitng./

---
+++ projectone/main.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var path = require('path');
+
+const port = process.env.PORT || 8081;
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://127.0.0.1:${port}`);
+});

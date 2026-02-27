f55d1771db797745e9b3876eda99b7332fbbbf84
Chris Gilchrist <chris@quix.ai>
2026-02-27 13:34:31 +0000

    Added 2 new projects.

---
+++ crypto-stream-dashboard/main.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var path = require('path');
+const port = process.env.PORT || 8081;
+var app = express();
+module.exports = app;
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname, 'index.html')); });
+app.listen(port, () => console.log(`Server running at http://127.0.0.1:${port}`));
